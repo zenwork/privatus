a794d4852585b2c6708a480abecafb7bc1fc10e1
zework <florian@hehlen.name>
2023-03-03 18:26:33 +0100

    added deno and deno-deploy support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
-export default defineConfig({});
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: deno()
+});
