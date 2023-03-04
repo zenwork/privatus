a3f6bb8d4a200e629bcd46ba741b553539531ad0
zework <florian@hehlen.name>
2023-03-04 01:25:46 +0100

    enable lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import {defineConfig} from 'astro/config'
 import deno           from '@astrojs/deno'
 import lit            from '@astrojs/lit'
+import {defineConfig} from 'astro/config'
 
 export default defineConfig({
   output: 'server',
   adapter: deno(),
-  // integrations: [lit()]
+  integrations: [lit()]
 });
