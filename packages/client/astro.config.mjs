f200b63a9af3b6956db315499ab301b1e61a3350
Florian Hehlen <florian@hehlen.name>
2023-03-10 08:00:16 +0100

    Setup multi package (#3)
    * moved files
    * set-up changesets
    * added deno server
    * changed to deploy server

--- packages/client/astro.config.mjs
+++ packages/client/astro.config.mjs
@@ -1,11 +1,11 @@
-import deno           from '@astrojs/deno'
+// import deno           from '@astrojs/deno'
 import lit            from '@astrojs/lit'
 import {defineConfig} from 'astro/config'
 
 export default defineConfig(
     {
         output:'server',
-        adapter:deno(),
+        // adapter:deno(),
         integrations:[lit()]
     }
 )
