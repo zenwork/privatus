547959753d4f6eddc7201e20ef419ff193a6fb9a
zework <florian@hehlen.name>
2023-03-11 16:05:47 +0100

    fix esbuild setup

--- packages/server/serve.ts
+++ packages/server/serve.ts
@@ -1,8 +1,26 @@
-import {serve} from 'https://deno.land/x/esbuild_serve/mod.ts'
+import {existsSync}   from 'https://deno.land/std@0.177.0/node/fs.ts'
+import * as esbuild   from 'https://deno.land/x/esbuild@v0.17.11/mod.js'
+import * as importMap from 'npm:esbuild-plugin-import-map'
 
-serve({
-    port: 8081,
-    pages: {
-        'index': './src/client/index.ts'
-    }
+if (!existsSync('./dist')) await Deno.mkdir('./dist', {recursive: true})
+importMap.load('./import_map.json')
+
+console.log('esbuild')
+await esbuild.build({
+    entryPoints: ['./src/client/index.ts'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    platform: 'neutral',
+    plugins: [importMap.plugin()],
+    outdir: './dist',
 })
+
+console.log('copy')
+await Deno.copyFile('./templates/index.html', './dist/index.html')
+await Deno.copyFile('./templates/index.css', './dist/index.css')
+console.log('copy')
+await Deno.copyFile('./templates/favicon.ico', './dist/favicon.ico')
+
+console.log('done')
+Deno.exit(0)
--- packages/server/src/client/index.ts
+++ packages/server/src/client/index.ts
@@ -1,1 +1,3 @@
 import './components/index.ts'
+
+console.log('foo bar')
