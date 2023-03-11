43e26fd24c4081c202fc17e480231e64556a69e9
zework <florian@hehlen.name>
2023-03-11 17:32:18 +0100

    debug: add debug asset

--- packages/server/main.ts
+++ packages/server/main.ts
@@ -29,31 +29,36 @@ app.use(async (context, next) => {
     let pathname = context.request.url.pathname
     if (pathname.indexOf('/api') > -1 || pathname.indexOf('/docs') > -1) {
         next()
-    }
-    let filepath = pathname === '/' ? '/index.html' : pathname
-    let assetPath = `${Deno.cwd()}/dist${filepath}`
-    // console.log(assetPath)
-    context.response.body = Deno.readFileSync(assetPath)
-    let extension = assetPath.substring(assetPath.lastIndexOf('.') + 1)
-    // console.log(extension)
-    switch (extension) {
-        case 'html':
-            context.response.type = 'text/html'
-            break
-        case 'css':
-            context.response.type = 'text/css'
-            break
-        case 'js':
-            context.response.type = 'application/javascript'
-            break
-        case 'json':
-            context.response.type = 'application/json'
-            break
-        case 'ico':
-            context.response.type = 'image/x-icon'
-            break
-        default:
-            context.response.type = 'test/html'
+    } else if (pathname !== '/debug.txt') {
+
+        let filepath = pathname === '/' ? '/index.html' : pathname
+        let assetPath = `${Deno.cwd()}/dist${filepath}`
+        // console.log(assetPath)
+        context.response.body = Deno.readFileSync(assetPath)
+        let extension = assetPath.substring(assetPath.lastIndexOf('.') + 1)
+        // console.log(extension)
+        switch (extension) {
+            case 'html':
+                context.response.type = 'text/html'
+                break
+            case 'css':
+                context.response.type = 'text/css'
+                break
+            case 'js':
+                context.response.type = 'application/javascript'
+                break
+            case 'json':
+                context.response.type = 'application/json'
+                break
+            case 'ico':
+                context.response.type = 'image/x-icon'
+                break
+            default:
+                context.response.type = 'text/plain'
+        }
+    } else {
+        context.response.body = 'DEBUG'
+        context.response.type = 'text/plain'
     }
 
 })
