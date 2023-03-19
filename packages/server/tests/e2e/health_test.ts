9177dd529f5891c8bfd692e4c90cbbaa8436ecf4
zework <florian@hehlen.name>
2023-03-19 10:24:18 +0100

    try to fix testing error in github actions

--- packages/server/tests/e2e/health_test.ts
+++ packages/server/tests/e2e/health_test.ts
@@ -1,11 +1,11 @@
-import {delay}        from 'deno/std/async/delay.ts'
-import {Application}  from 'oak'
-import {superoak}     from 'superoak'
+import { delay } from 'deno/std/async/delay.ts'
+import { Application } from 'oak'
+import { superoak } from 'superoak'
 
 import '../../src/build/build.ts'
-import {initBackend}  from '../../src/server/initBackend.ts'
-import {initFrontend} from '../../src/server/initFrontend.ts'
-import {create}       from '../../src/server/server.ts'
+import { initBackend } from '../../src/server/initBackend.ts'
+import { initFrontend } from '../../src/server/initFrontend.ts'
+import { create } from '../../src/server/server.ts'
 
 Deno.test(
     'health test frontend',
@@ -23,7 +23,6 @@ Deno.test(
             await request.get('/')
                 .expect(200)
                 .expect('Content-Type', 'text/html; charset=UTF-8')
-
         })
 
         await t.step('check assets', async () => {
@@ -32,18 +31,15 @@ Deno.test(
                 .expect(200)
                 .expect('Content-Type', 'text/html; charset=UTF-8')
 
-
             request = await superoak(app)
             await request.get('/index.css')
                 .expect(200)
                 .expect('Content-Type', 'text/css; charset=UTF-8')
 
-
             request = await superoak(app)
             await request.get('/index.js')
                 .expect(200)
                 .expect('Content-Type', 'application/javascript; charset=UTF-8')
-
 
             await delay(1000)
         })
@@ -66,7 +62,7 @@ Deno.test(
             await request.get('/api')
                 .expect(200)
                 .expect('Content-Type', 'application/json; charset=UTF-8')
-                .expect({status: 'OK'})
+                .expect({ status: 'OK' })
         })
 
         await t.step('check docs', async () => {
