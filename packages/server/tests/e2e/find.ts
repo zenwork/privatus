4af8fafc8ea81018c701687115568e6643cc2b26
zework <florian@hehlen.name>
2023-03-22 23:02:53 +0100

    open wc spike setup

--- packages/server/tests/e2e/find.ts
+++ packages/server/tests/e2e/find.ts
@@ -1,49 +1,49 @@
-import * as path from 'deno/std/path/mod.ts';
+import * as path from 'deno/std/path/mod.ts'
 
-const IGNORED_DIRECTORIES = new Set(['.git']);
+const IGNORED_DIRECTORIES = new Set(['.git'])
 
 interface FilterOptions {
-  extension?: string;
+    extension?: string
 }
 
 export async function getFilesList(
-  directory: string,
-  options: FilterOptions = {}
+    directory: string,
+    options: FilterOptions = {},
 ): Promise<string[]> {
-  const foundFiles: string[] = [];
-  for await (const fileOrFolder of Deno.readDir(directory)) {
-    if (fileOrFolder.isDirectory) {
-      if (IGNORED_DIRECTORIES.has(fileOrFolder.name)) {
-        // Skip this folder, it's in the ignore list.
-        continue;
-      }
-      // If it's not ignored, recurse and search this folder for files.
-      const nestedFiles = await getFilesList(
-        path.join(directory, fileOrFolder.name),
-        options
-      );
-      foundFiles.push(...nestedFiles);
-    } else {
-      // We know it's a file, and not a folder.
+    const foundFiles: string[] = []
+    for await (const fileOrFolder of Deno.readDir(directory)) {
+        if (fileOrFolder.isDirectory) {
+            if (IGNORED_DIRECTORIES.has(fileOrFolder.name)) {
+                // Skip this folder, it's in the ignore list.
+                continue
+            }
+            // If it's not ignored, recurse and search this folder for files.
+            const nestedFiles = await getFilesList(
+                path.join(directory, fileOrFolder.name),
+                options,
+            )
+            foundFiles.push(...nestedFiles)
+        } else {
+            // We know it's a file, and not a folder.
 
-      // True if we weren't given an extension to filter, or if we were and the file's extension matches the provided filter.
-      const shouldStoreFile = !options.extension ||
-                              path.extname(fileOrFolder.name) === `.${options.extension}`;
+            // True if we weren't given an extension to filter, or if we were and the file's extension matches the provided filter.
+            const shouldStoreFile = !options.extension ||
+                path.extname(fileOrFolder.name) === `.${options.extension}`
 
-      if (shouldStoreFile) {
-        foundFiles.push(path.join(directory, fileOrFolder.name));
-      }
+            if (shouldStoreFile) {
+                foundFiles.push(path.join(directory, fileOrFolder.name))
+            }
+        }
     }
-  }
-  return foundFiles;
+    return foundFiles
 }
 
-export async function getFirstFileName(
-  directory: string,
-  options: FilterOptions = {}
+export function getFirstFileName(
+    directory: string,
+    options: FilterOptions = {},
 ): Promise<string> {
-  return getFilesList(directory, options).then(files => {
-    const filename = files[0];
-    return filename.substring(filename.lastIndexOf('/'));
-  });
+    return getFilesList(directory, options).then((files) => {
+        const filename = files[0]
+        return filename.substring(filename.lastIndexOf('/'))
+    })
 }
--- packages/server/tests/e2e/health_test.ts
+++ packages/server/tests/e2e/health_test.ts
@@ -1,88 +1,88 @@
-import { delay }       from 'deno/std/async/delay.ts';
-import { Application } from 'oak';
-import { superoak }    from 'superoak';
+import { delay } from 'deno/std/async/delay.ts'
+import { Application } from 'oak'
+import { superoak } from 'superoak'
 
-import { initBackend }  from '../../src/server/initBackend.ts';
-import { initFrontend } from '../../src/server/initFrontend.ts';
-import { create }       from '../../src/server/server.ts';
+import { initBackend } from '../../src/server/initBackend.ts'
+import { initFrontend } from '../../src/server/initFrontend.ts'
+import { create } from '../../src/server/server.ts'
 
-import { describe, it }                   from 'deno/std/testing/bdd.ts';
-import { getFilesList, getFirstFileName } from './find.ts';
+import { describe, it } from 'deno/std/testing/bdd.ts'
+import { getFirstFileName } from './find.ts'
 
-const clientDistDir = `${Deno.cwd()}/../client/dist`;
+const clientDistDir = `${Deno.cwd()}/../client/dist`
 
 describe({
-  name: 'health',
-  fn: () => {
-    describe(
-      'frontend',
-      () => {
-        let app: Application;
+    name: 'health',
+    fn: () => {
+        describe(
+            'frontend',
+            () => {
+                let app: Application
 
-        it('init', () => {
-          app = create((app: Application) => {
-            initFrontend(app, clientDistDir);
-          }).app;
-        });
+                it('init', () => {
+                    app = create((app: Application) => {
+                        initFrontend(app, clientDistDir)
+                    }).app
+                })
 
-        it('check root', async () => {
-          const request = await superoak(app);
-          await request.get('/')
-            .expect(200)
-            .expect('Content-Type', 'text/html; charset=UTF-8');
-        });
+                it('check root', async () => {
+                    const request = await superoak(app)
+                    await request.get('/')
+                        .expect(200)
+                        .expect('Content-Type', 'text/html; charset=UTF-8')
+                })
 
-        it('check assets', async () => {
-          let request = await superoak(app);
-          await request.get('/index.html')
-            .expect(200)
-            .expect('Content-Type', 'text/html; charset=UTF-8');
+                it('check assets', async () => {
+                    let request = await superoak(app)
+                    await request.get('/index.html')
+                        .expect(200)
+                        .expect('Content-Type', 'text/html; charset=UTF-8')
 
-          request = await superoak(app);
-          const cssFile = await getFirstFileName(clientDistDir, { extension: 'css' });
-          await request.get(cssFile)
-            .expect(200)
-            .expect('Content-Type', 'text/css; charset=UTF-8');
+                    request = await superoak(app)
+                    const cssFile = await getFirstFileName(clientDistDir, { extension: 'css' })
+                    await request.get(cssFile)
+                        .expect(200)
+                        .expect('Content-Type', 'text/css; charset=UTF-8')
 
-          request = await superoak(app);
-          const jsFile = await getFirstFileName(clientDistDir, { extension: 'js' });
-          ;
-          await request.get(jsFile)
-            .expect(200)
-            .expect('Content-Type', 'application/javascript; charset=UTF-8');
+                    request = await superoak(app)
+                    const jsFile = await getFirstFileName(clientDistDir, { extension: 'js' })
 
-          await delay(1000);
-        });
-      }
-    );
+                    await request.get(jsFile)
+                        .expect(200)
+                        .expect('Content-Type', 'application/javascript; charset=UTF-8')
 
-    describe(
-      'backend',
-      () => {
-        let app: Application;
+                    await delay(1000)
+                })
+            },
+        )
 
-        it('init', () => {
-          app = create((app: Application) => {
-            initBackend(app);
-          }).app;
-        });
+        describe(
+            'backend',
+            () => {
+                let app: Application
 
-        it('check api', async () => {
-          const request = await superoak(app);
-          await request.get('/api')
-            .expect(200)
-            .expect('Content-Type', 'application/json; charset=UTF-8')
-            .expect({ status: 'OK' });
-        });
+                it('init', () => {
+                    app = create((app: Application) => {
+                        initBackend(app)
+                    }).app
+                })
 
-        it('check docs', async () => {
-          const request = await superoak(app);
-          await request.get('/api/docs')
-            .expect(200)
-            .expect('Content-Type', 'text/html');
-        });
-      }
-    );
-  },
-  sanitizeOps: false
-});
+                it('check api', async () => {
+                    const request = await superoak(app)
+                    await request.get('/api')
+                        .expect(200)
+                        .expect('Content-Type', 'application/json; charset=UTF-8')
+                        .expect({ status: 'OK' })
+                })
+
+                it('check docs', async () => {
+                    const request = await superoak(app)
+                    await request.get('/api/docs')
+                        .expect(200)
+                        .expect('Content-Type', 'text/html')
+                })
+            },
+        )
+    },
+    sanitizeOps: false,
+})
