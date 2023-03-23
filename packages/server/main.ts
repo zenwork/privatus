35734c04ab1fe98adf42560c13def78b3a291733
zework <florian@hehlen.name>
2023-03-23 21:33:19 +0100

    open wc spike setup

--- packages/server/main.ts
+++ packages/server/main.ts
@@ -4,7 +4,7 @@ import { initFrontend } from './src/server/initFrontend.ts'
 import { create }       from './src/server/server.ts'
 
 console.log('STARTING')
-const clientDir = `${Deno.cwd()}/../client/dist`
+const clientDir = `${Deno.cwd()}/client`
 
 await create((app: Application<Record<string, any>>) => {
     initBackend(app)
