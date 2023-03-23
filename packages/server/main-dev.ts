0e5dd76ef189cbcf07444f2ac56c147c9bb98d78
zework <florian@hehlen.name>
2023-03-23 17:18:45 +0100

    open wc spike setup

--- packages/server/main-dev.ts
+++ packages/server/main-dev.ts
@@ -1,6 +1,5 @@
 import { Application }  from 'oak';
 import { initBackend }  from './src/server/initBackend.ts';
-import { initDevMode }  from './src/server/initDevMode.ts';
 import { initFrontend } from './src/server/initFrontend.ts';
 import { create }       from './src/server/server.ts';
 
--- packages/server/src/server/initDevMode.ts
+++
@@ -1,6 +0,0 @@
-import { esBuildWatch } from '../build/dev.ts'
-
-export function initDevMode() {
-    console.log('start in dev mode')
-    esBuildWatch()
-}
