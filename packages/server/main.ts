91cb01b5e6f50804cffeec3ff0a1c459fc847836
zework <florian@hehlen.name>
2023-05-15 17:48:45 +0200

    better error catching

--- packages/server/main.ts
+++ packages/server/main.ts
@@ -1,12 +1,17 @@
 import { Application } from 'oak'
+import { fallback } from './src/server/fallback.ts'
 import { initBackend } from './src/server/initBackend.ts'
 import { initFrontend } from './src/server/initFrontend.ts'
 import { create } from './src/server/server.ts'
 
-console.log('STARTING')
-const clientDir = `${Deno.cwd()}/server/client-dist`
+try {
+  console.log('STARTING')
+  const clientDir = `${Deno.cwd()}/server/client-dist`
 
-await create((app: Application<Record<string, any>>) => {
-  initBackend(app)
-  initFrontend(app, clientDir)
-}).startBlock()
+  await create((app: Application<Record<string, any>>) => {
+    initBackend(app)
+    initFrontend(app, clientDir)
+  }).startBlock()
+} catch (e) {
+  await fallback({ error: e.toString() })
+}
