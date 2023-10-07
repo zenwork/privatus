301a1e88e4da2d8f09414cc32c873df0873e2f79
Florian Hehlen <florian@hehlen.name>
2023-10-08 00:24:29 +0200

    refactor deploy check script (#33)
    * refactor workflow scripts
    * added duplicate action skipping logic
    * Split workflows into PR and MAIN scripts
    * Add conditional check before the deployment

--- tools/check.sh
+++ tools/check.sh
@@ -2,7 +2,7 @@ deno task checkDeployable
 RESULT=$?
 status="skip"
 if [ $RESULT -eq 0 ]; then
-  status="deploy"
+  status="true"
 fi
-echo "check:$status"
-echo "status=$status" >>$GITHUB_OUTPUT
+echo "deploy:$status"
+echo "deploy=$status" >>$GITHUB_OUTPUT
